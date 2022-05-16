c03df610a41a03f24cef56d81158f7e56472c0c8
Fer Ramirez <fer.eze.ram@gmail.com>
2022-05-16 02:16:27 -0300

    heroku configs

--- api/src/db.js
+++ api/src/db.js
@@ -4,13 +4,16 @@ const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-  {
-    logging: false,
-    native: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  logging: false,
+  native: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
