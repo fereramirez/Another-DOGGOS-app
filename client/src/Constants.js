174cec6c88b9f6565ffdb9a2191e2dfcac3e4eb5
Fer Ramirez <fer.eze.ram@gmail.com>
2022-05-16 02:46:00 -0300

    heroku configs

--- client/src/Constants.js
+++ client/src/Constants.js
@@ -1,5 +1,5 @@
-const URL = "http://localhost:3001/dog/";
-const URL_NAME = "http://localhost:3001/dog/search?name=";
+const URL = "https://another-dogos-app.herokuapp.com/dog/";
+const URL_NAME = "https://another-dogos-app.herokuapp.com/dog/search?name=";
 const dogsPerPage = 12;
 
 export { URL, URL_NAME, dogsPerPage };
