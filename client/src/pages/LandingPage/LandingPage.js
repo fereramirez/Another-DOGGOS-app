0328c124d31f3624d280d930bda03715bfd20c96
Fer Ramirez <fer.eze.ram@gmail.com>
2022-05-16 00:39:39 -0300

    landing video mobile added

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -3,9 +3,25 @@ import "./LandingPage.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { faPaw } from "@fortawesome/free-solid-svg-icons";
-import video from "../../Assets/dogs-walking.mp4";
+import videoDesktop from "../../Assets/dogs-walking.mp4";
+import videoMobile from "../../Assets/dogs-stick.mp4";
+import { useEffect, useState } from "react";
 
 const LandingPage = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  function handleWindowSizeChange() {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  const isMobile = width <= 700;
+
   return (
     <>
       <div className="landing-container">
@@ -28,9 +44,15 @@ const LandingPage = () => {
           </p>
         </div>
       </div>
-      <video className="landing-video" autoPlay loop muted>
-        <source src={video} type="video/mp4" />
-      </video>
+      {isMobile ? (
+        <video className="landing-video" autoPlay loop muted>
+          <source src={videoMobile} type="video/mp4" />
+        </video>
+      ) : (
+        <video className="landing-video" autoPlay loop muted>
+          <source src={videoDesktop} type="video/mp4" />
+        </video>
+      )}
     </>
   );
 };
